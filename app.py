d033ad9b040e88c3d12c5bac8bd911b04b66b95b
LelandEdwards <97922360+LelandEdwards@users.noreply.github.com>
2023-04-16 10:59:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 print('Hello, world!')
-print('We really don\'t know how to code')
-print('We found out this was a neural network and not a machine learning model way too late')
+print('We really don\'t know how to code :/')
+print('We found out this was a neural network and not a machine learning model way too late.')
 
 for i in range(20):   
     print("Hello, world!")
